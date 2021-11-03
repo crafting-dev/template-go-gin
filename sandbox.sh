fb727804fdcdafa444b940346523ad21c0af3159
Alexander Maru <i@alexandermaru.com>
2021-11-02 21:28:44 -0400

    Update script

--- sandbox.sh
+++ sandbox.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# ============================================
+# Configure sandbox for quick workspace setup.
+# ============================================
+
 mkdir -p build
 go build -o build/server main.go
